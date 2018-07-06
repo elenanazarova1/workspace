3ff9ede7a47adcebd1cc385797ba29c196372a96
Galicina <lelagalicina@gmail.com>
2018-07-06 21:33:24 +0300

    2

---
+++ auto_test/src/main/java/auto_test/Square.java
@@ -0,0 +1,20 @@
+package auto_test;
+
+public class Square {
+	
+	public double l;
+	
+	public Square(double l) {
+		this.l = l;
+				
+	}
+	
+	public double area() {
+		return this.l * this.l;
+	}
+	
+	public double square() {
+		return this.l * this.l;
+	}
+
+}
