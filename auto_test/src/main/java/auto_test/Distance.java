16e15e9b1f3025576269c5706ab9d656ce3ce81c
Galicina <lelagalicina@gmail.com>
2018-07-06 21:53:10 +0300

    3

--- auto_test/src/main/java/auto_test/Distance.java
+++ auto_test/src/main/java/auto_test/Distance.java
@@ -1,5 +1,18 @@
 package auto_test;
 
 public class Distance {
+	
+	public double x;
+	public double y;
 
+	public Distance(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double distance() {
+	
+		return Math.sqrt((this.x * this.x) + (this.y * this.y));
+	}
+	
 }
--- auto_test/src/main/java/auto_test/First.java
+++ auto_test/src/main/java/auto_test/First.java
@@ -2,11 +2,23 @@ package auto_test;
 
 public class First {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-		int l = 6;
-		int s = l*l;
-		System.out.println("Площадь со стороной " + l + " = " + s);
+	
+	 public static void main(String[] args) {
+	 
+		
+		Distance d = new Distance(5, 7);
+				
+		System.out.println("Расстояние между двумя точками Point p1 = " + d.x + " и Point p2 = " + d.y + " равно " + d.distance());
+		
+		
+		Square s = new Square(6);
+		
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.square());
+		
+		
+		Rectangle r = new Rectangle(3, 8);
+		
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.rectangle());
 		
 	}
 
